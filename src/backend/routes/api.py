fb5e5e288dc980f02f0805bac79e0680b700c356
George <george.neokq@gmail.com>
2022-02-23 16:44:22 +0800

    WIP: Files cleanup

--- src/backend/routes/api.py
+++ src/backend/routes/api.py
@@ -1,7 +1,17 @@
 from tempfile import gettempdir
 from flask import Flask, request
-from os import path
+from os import path, getenv
 from backend.features.convert import BadParamError, InvalidLinkError, convert_audio
+from threading import Thread
+
+cleanup_interval = getenv('CLEANUP_INTERVAL_HOURS') or 10
+
+"""
+Clean up old audio files, from 10 hours ago
+"""
+def cleanup():
+    # Read all files from storage folder
+    pass
 
 def declare_api_routes(app: Flask):
     @app.route('/api/convert/audio', methods=['POST'])
@@ -14,6 +24,8 @@ def declare_api_routes(app: Flask):
             info = convert_audio(link, start_timestamp, end_timestamp, destination)
         except (InvalidLinkError, BadParamError) as e:
             return {'error': str(e)}, 422  # Unprocessible entity
+
+        Thread(target=cleanup).start()
 
         return {
             **info,
